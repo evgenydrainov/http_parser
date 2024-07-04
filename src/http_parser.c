3155361d97d10b931e99108bc58867afb7563d53
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2024-07-04 18:33:51 +0300

    Implement PARSING_RES_NOT_ENOUGH_DATA

--- src/http_parser.c
+++ src/http_parser.c
@@ -28,6 +28,23 @@ static string eat_line(string* str) {
 	return result;
 }
 
+static string eat_header_name(string* str) {
+	string result = {str->data, 0};
+	while (*str->data != ':' && str->count > 0) {
+		str->data++;
+		str->count--;
+		result.count++;
+	}
+
+	// Skip colon.
+	if (str->count > 0) {
+		str->data++;
+		str->count--;
+	}
+
+	return result;
+}
+
 static bool is_whitespace(char ch) {
 	return ch == ' ' || ch == '\t' || ch == '\n'; // Should '\r' be here?
 }
@@ -53,16 +70,19 @@ static void eat_whitespace(string* str) {
 	}
 }
 
-static uint16_t string_to_u16(string str) {
+static uint16_t string_to_u16(string str, bool* done) {
 	uint16_t result = 0;
 	for (size_t i = 0; i < str.count; i++) {
 		if (!is_numerical(str.data[i])) {
+			*done = false;
 			return 0;
 		}
 
 		result *= 10;
 		result += str.data[i] - '0';
 	}
+
+	*done = true;
 	return result;
 }
 
@@ -76,6 +96,15 @@ static bool strings_match(string a, string b) {
 		}
 	}
 	return true;
+}
+
+static bool string_contains_char(string str, char ch) {
+	for (size_t i = 0; i < str.count; i++) {
+		if (str.data[i] == ch) {
+			return true;
+		}
+	}
+	return false;
 }
 
 http_parsing_result_t http_parse_response(const char *text_data, size_t text_len,
@@ -97,6 +126,10 @@ http_parsing_result_t http_parse_response(const char *text_data, size_t text_len
 		eat_whitespace(&status_line);
 		string protocol_version = eat_word(&status_line);
 		
+		if (protocol_version.count == 0) {
+			return PARSING_RES_NOT_ENOUGH_DATA;
+		}
+
 		if (!(strings_match(protocol_version, STR("HTTP/1.1")) || strings_match(protocol_version, STR("HTTP/1.0")))) {
 			// Unknown protocol version
 			return PARSING_RES_FAILED;
@@ -109,12 +142,21 @@ http_parsing_result_t http_parse_response(const char *text_data, size_t text_len
 		eat_whitespace(&status_line);
 		string status_code = eat_word(&status_line);
 
-		out_resp->status_code = string_to_u16(status_code);
-		// TODO: if status code is not integer, then error
+		if (status_code.count == 0) {
+			return PARSING_RES_NOT_ENOUGH_DATA;
+		}
+
+		bool done;
+		out_resp->status_code = string_to_u16(status_code, &done);
+		if (!done) {
+			return PARSING_RES_FAILED;
+		}
 
 		// Remaining line is the status text.
 		eat_whitespace(&status_line);
 		string status_text = status_line;
+
+		// Maybe status text can be empty, so don't error
 
 		out_resp->status_text     = status_text.data;
 		out_resp->status_text_len = status_text.count;
@@ -128,18 +170,23 @@ http_parsing_result_t http_parse_response(const char *text_data, size_t text_len
 
 		while (header_line.count > 0) {
 			eat_whitespace(&header_line);
-			string header_name = eat_word(&header_line);
 
-			// Strip colon in header name.
-			if (header_name.count > 0) {
-				header_name.count--;
-			} else {
-				// Invalid header name.
-				return PARSING_RES_FAILED;
+			if (!string_contains_char(header_line, ':')) {
+				return PARSING_RES_NOT_ENOUGH_DATA;
+			}
+
+			string header_name = eat_header_name(&header_line);
+
+			if (header_name.count == 0) {
+				return PARSING_RES_NOT_ENOUGH_DATA;
 			}
 
 			eat_whitespace(&header_line);
 			string header_value = header_line;
+
+			if (header_value.count == 0) {
+				return PARSING_RES_NOT_ENOUGH_DATA;
+			}
 
 			if (header_index < headers_max_len) {
 				headers_buf[header_index].name     = header_name.data;
@@ -165,6 +212,8 @@ http_parsing_result_t http_parse_response(const char *text_data, size_t text_len
 	// Remaining text is the body.
 	{
 		string body = text;
+
+		// Body is optional, so don't check for empty?
 
 		out_resp->body     = body.data;
 		out_resp->body_len = body.count;
