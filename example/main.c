2075a802528f66d6e5e9417ba7556f3a11e32ee5
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2024-07-08 17:55:41 +0300

    Change indentation to spaces

--- example/main.c
+++ example/main.c
@@ -6,101 +6,101 @@
 #define ARRAY_LENGTH(a) (sizeof(a) / sizeof(a[0]))
 
 static void check_http_parse_response(const char* text, size_t text_len,
-									  http_parsing_result_t expect_result) {
-	http_header_t headers_buf[100];
+                                      http_parsing_result_t expect_result) {
+    http_header_t headers_buf[100];
 
-	http_response_t response;
-	http_parsing_result_t result = http_parse_response(text, text_len,
-													   headers_buf, ARRAY_LENGTH(headers_buf),
-													   &response);
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, text_len,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
 
-	assert(result == expect_result && "Test failed.");
-	if (result != expect_result) {
-		printf("Test failed.\n");
-		exit(1);
-	}
+    assert(result == expect_result && "Test failed.");
+    if (result != expect_result) {
+        printf("Test failed.\n");
+        exit(1);
+    }
 
-	/*
-	printf("Result: %s\n", translate_http_parsing_result(result));
+    /*
+    printf("Result: %s\n", translate_http_parsing_result(result));
 
-	printf("Protocol: %.*s\n", response.protocol_len, response.protocol);
-	printf("Status Code: %u\n", response.status_code);
-	printf("Status Text: %.*s\n", response.status_text_len, response.status_text);
+    printf("Protocol: %.*s\n", response.protocol_len, response.protocol);
+    printf("Status Code: %u\n", response.status_code);
+    printf("Status Text: %.*s\n", response.status_text_len, response.status_text);
 
-	printf("Headers: \n");
-	for (size_t i = 0; i < response.headers_len; i++) {
-		http_header_t h = response.headers[i];
-		printf("%zu: %.*s: %.*s\n", i, h.name_len, h.name, h.value_len, h.value);
-	}
+    printf("Headers: \n");
+    for (size_t i = 0; i < response.headers_len; i++) {
+        http_header_t h = response.headers[i];
+        printf("%zu: %.*s: %.*s\n", i, h.name_len, h.name, h.value_len, h.value);
+    }
 
-	printf("Body: %.*s\n", response.body_len, response.body);
-	*/
+    printf("Body: %.*s\n", response.body_len, response.body);
+    */
 
-	printf("Test passed.\n");
+    printf("Test passed.\n");
 }
 
 int main(void) {
-	char http_response_full[] =
-		"HTTP/1.1 403 Forbidden\n"
-		"Server: Apache\n"
-		"Content-Type: text/html; charset=iso-8859-1\n"
-		"Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-		"Keep-Alive: timeout-=5, max=1000\n"
-		"Connection: Keep-Alive\n"
-		"Age: 3464\n"
-		"Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-		"X-Cache-Info: caching\n"
-		"Content-Length: 220\n"
-		"\n"
-		"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-		"2.0//EN\">\n"
-		"(more data)\n";
+    char http_response_full[] =
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)\n";
 
-	char http_response_empty[] = "";
+    char http_response_empty[] = "";
 
-	char http_response_no_body[] =
-		"HTTP/1.1 403 Forbidden\n"
-		"Server: Apache\n"
-		"Content-Type: text/html; charset=iso-8859-1\n"
-		"Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-		"Keep-Alive: timeout-=5, max=1000\n"
-		"Connection: Keep-Alive\n"
-		"Age: 3464\n"
-		"Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-		"X-Cache-Info: caching\n"
-		"Content-Length: 220\n";
-	
-	char http_response_bad_header[] =
-		"HTTP/1.1 403 Forbidden\n"
-		"Server: Apache\n"
-		"Content-Type: text/html; charset=iso-8859-1\n"
-		"Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-		"Keep-Alive: timeout-=5, max=1000\n"
-		"Conn";
-	
-	char http_response_bad_status[] =
-		"HTTP/1.1 FOOBAR Forbidden\n"
-		"Server: Apache\n"
-		"Content-Type: text/html; charset=iso-8859-1\n"
-		"Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-		"Keep-Alive: timeout-=5, max=1000\n"
-		"Connection: Keep-Alive\n"
-		"Age: 3464\n"
-		"Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-		"X-Cache-Info: caching\n"
-		"Content-Length: 220\n"
-		"\n"
-		"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-		"2.0//EN\">\n"
-		"(more data)\n";
+    char http_response_no_body[] =
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n";
+    
+    char http_response_bad_header[] =
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Conn";
+    
+    char http_response_bad_status[] =
+        "HTTP/1.1 FOOBAR Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)\n";
 
-	char http_response_only_protocol[] =
-		"HTTP/1.1";
+    char http_response_only_protocol[] =
+        "HTTP/1.1";
 
-	check_http_parse_response(http_response_full,          sizeof(http_response_full)          - 1, PARSING_RES_SUCCEEDED);
-	check_http_parse_response(http_response_empty,         sizeof(http_response_empty)         - 1, PARSING_RES_NOT_ENOUGH_DATA);
-	check_http_parse_response(http_response_no_body,       sizeof(http_response_no_body)       - 1, PARSING_RES_SUCCEEDED);
-	check_http_parse_response(http_response_bad_header,    sizeof(http_response_bad_header)    - 1, PARSING_RES_NOT_ENOUGH_DATA);
-	check_http_parse_response(http_response_bad_status,    sizeof(http_response_bad_status)    - 1, PARSING_RES_FAILED);
-	check_http_parse_response(http_response_only_protocol, sizeof(http_response_only_protocol) - 1, PARSING_RES_NOT_ENOUGH_DATA);
+    check_http_parse_response(http_response_full,          sizeof(http_response_full)          - 1, PARSING_RES_SUCCEEDED);
+    check_http_parse_response(http_response_empty,         sizeof(http_response_empty)         - 1, PARSING_RES_NOT_ENOUGH_DATA);
+    check_http_parse_response(http_response_no_body,       sizeof(http_response_no_body)       - 1, PARSING_RES_SUCCEEDED);
+    check_http_parse_response(http_response_bad_header,    sizeof(http_response_bad_header)    - 1, PARSING_RES_NOT_ENOUGH_DATA);
+    check_http_parse_response(http_response_bad_status,    sizeof(http_response_bad_status)    - 1, PARSING_RES_FAILED);
+    check_http_parse_response(http_response_only_protocol, sizeof(http_response_only_protocol) - 1, PARSING_RES_NOT_ENOUGH_DATA);
 }
--- src/http_parser.c
+++ src/http_parser.c
@@ -5,219 +5,219 @@
 #define STR(s) ((string) {s, sizeof(s) - 1})
 
 typedef struct {
-	const char* data;
-	size_t count;
+    const char* data;
+    size_t count;
 } string;
 
 // Only LF
 // TODO: handle CRLF and CR
 static string eat_line(string* str) {
-	string result = {str->data, 0};
-	while (str->count > 0 && *str->data != '\n') {
-		str->data++;
-		str->count--;
-		result.count++;
-	}
+    string result = {str->data, 0};
+    while (str->count > 0 && *str->data != '\n') {
+        str->data++;
+        str->count--;
+        result.count++;
+    }
 
-	// Skip newline character.
-	if (str->count > 0) {
-		str->data++;
-		str->count--;
-	}
+    // Skip newline character.
+    if (str->count > 0) {
+        str->data++;
+        str->count--;
+    }
 
-	return result;
+    return result;
 }
 
 static string eat_header_name(string* str) {
-	string result = {str->data, 0};
-	while (str->count > 0 && *str->data != ':') {
-		str->data++;
-		str->count--;
-		result.count++;
-	}
+    string result = {str->data, 0};
+    while (str->count > 0 && *str->data != ':') {
+        str->data++;
+        str->count--;
+        result.count++;
+    }
 
-	// Skip colon.
-	if (str->count > 0) {
-		str->data++;
-		str->count--;
-	}
+    // Skip colon.
+    if (str->count > 0) {
+        str->data++;
+        str->count--;
+    }
 
-	return result;
+    return result;
 }
 
 static bool is_whitespace(char ch) {
-	return ch == ' ' || ch == '\t' || ch == '\n'; // Should '\r' be here?
+    return ch == ' ' || ch == '\t' || ch == '\n'; // Should '\r' be here?
 }
 
 static bool is_numerical(char ch) {
-	return ch >= '0' && ch <= '9';
+    return ch >= '0' && ch <= '9';
 }
 
 static string eat_word(string* str) {
-	string result = {str->data, 0};
-	while (str->count > 0 && !is_whitespace(*str->data)) {
-		str->data++;
-		str->count--;
-		result.count++;
-	}
-	return result;
+    string result = {str->data, 0};
+    while (str->count > 0 && !is_whitespace(*str->data)) {
+        str->data++;
+        str->count--;
+        result.count++;
+    }
+    return result;
 }
 
 static void eat_whitespace(string* str) {
-	while (str->count > 0 && is_whitespace(*str->data)) {
-		str->data++;
-		str->count--;
-	}
+    while (str->count > 0 && is_whitespace(*str->data)) {
+        str->data++;
+        str->count--;
+    }
 }
 
 static uint16_t string_to_u16(string str, bool* done) {
-	uint16_t result = 0;
-	for (size_t i = 0; i < str.count; i++) {
-		if (!is_numerical(str.data[i])) {
-			*done = false;
-			return 0;
-		}
+    uint16_t result = 0;
+    for (size_t i = 0; i < str.count; i++) {
+        if (!is_numerical(str.data[i])) {
+            *done = false;
+            return 0;
+        }
 
-		result *= 10;
-		result += str.data[i] - '0';
-	}
+        result *= 10;
+        result += str.data[i] - '0';
+    }
 
-	*done = true;
-	return result;
+    *done = true;
+    return result;
 }
 
 static bool strings_match(string a, string b) {
-	if (a.count != b.count) {
-		return false;
-	}
-	for (size_t i = 0; i < a.count; i++) {
-		if (a.data[i] != b.data[i]) {
-			return false;
-		}
-	}
-	return true;
+    if (a.count != b.count) {
+        return false;
+    }
+    for (size_t i = 0; i < a.count; i++) {
+        if (a.data[i] != b.data[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 static bool string_contains_char(string str, char ch) {
-	for (size_t i = 0; i < str.count; i++) {
-		if (str.data[i] == ch) {
-			return true;
-		}
-	}
-	return false;
+    for (size_t i = 0; i < str.count; i++) {
+        if (str.data[i] == ch) {
+            return true;
+        }
+    }
+    return false;
 }
 
 http_parsing_result_t http_parse_response(const char *text_data, size_t text_len,
-										  http_header_t *headers_buf, size_t headers_max_len,
-										  http_response_t *out_resp) {
-	if (!text_data || !headers_buf || !out_resp) {
-		return PARSING_RES_FAILED;
-	}
+                                          http_header_t *headers_buf, size_t headers_max_len,
+                                          http_response_t *out_resp) {
+    if (!text_data || !headers_buf || !out_resp) {
+        return PARSING_RES_FAILED;
+    }
 
-	*out_resp = (http_response_t) {0};
-	
-	string text = {text_data, text_len};
+    *out_resp = (http_response_t) {0};
+    
+    string text = {text_data, text_len};
 
-	// Read status line.
-	{
-		string status_line = eat_line(&text);
+    // Read status line.
+    {
+        string status_line = eat_line(&text);
 
-		// Protocol version.
-		eat_whitespace(&status_line);
-		string protocol_version = eat_word(&status_line);
-		
-		if (protocol_version.count == 0) {
-			return PARSING_RES_NOT_ENOUGH_DATA;
-		}
+        // Protocol version.
+        eat_whitespace(&status_line);
+        string protocol_version = eat_word(&status_line);
+        
+        if (protocol_version.count == 0) {
+            return PARSING_RES_NOT_ENOUGH_DATA;
+        }
 
-		if (!(strings_match(protocol_version, STR("HTTP/1.1")) || strings_match(protocol_version, STR("HTTP/1.0")))) {
-			// Unknown protocol version
-			return PARSING_RES_FAILED;
-		}
+        if (!(strings_match(protocol_version, STR("HTTP/1.1")) || strings_match(protocol_version, STR("HTTP/1.0")))) {
+            // Unknown protocol version
+            return PARSING_RES_FAILED;
+        }
 
-		out_resp->protocol     = protocol_version.data;
-		out_resp->protocol_len = protocol_version.count;
+        out_resp->protocol     = protocol_version.data;
+        out_resp->protocol_len = protocol_version.count;
 
-		// Status code.
-		eat_whitespace(&status_line);
-		string status_code = eat_word(&status_line);
+        // Status code.
+        eat_whitespace(&status_line);
+        string status_code = eat_word(&status_line);
 
-		if (status_code.count == 0) {
-			return PARSING_RES_NOT_ENOUGH_DATA;
-		}
+        if (status_code.count == 0) {
+            return PARSING_RES_NOT_ENOUGH_DATA;
+        }
 
-		bool done;
-		out_resp->status_code = string_to_u16(status_code, &done);
-		if (!done) {
-			return PARSING_RES_FAILED;
-		}
+        bool done;
+        out_resp->status_code = string_to_u16(status_code, &done);
+        if (!done) {
+            return PARSING_RES_FAILED;
+        }
 
-		// Remaining line is the status text.
-		eat_whitespace(&status_line);
-		string status_text = status_line;
+        // Remaining line is the status text.
+        eat_whitespace(&status_line);
+        string status_text = status_line;
 
-		// Maybe status text can be empty, so don't error
+        // Maybe status text can be empty, so don't error
 
-		out_resp->status_text     = status_text.data;
-		out_resp->status_text_len = status_text.count;
-	}
+        out_resp->status_text     = status_text.data;
+        out_resp->status_text_len = status_text.count;
+    }
 
-	// Read headers.
-	{
-		string header_line = eat_line(&text);
-		size_t header_index = 0;
-		size_t num_headers_written = 0;
+    // Read headers.
+    {
+        string header_line = eat_line(&text);
+        size_t header_index = 0;
+        size_t num_headers_written = 0;
 
-		while (header_line.count > 0) {
-			eat_whitespace(&header_line);
+        while (header_line.count > 0) {
+            eat_whitespace(&header_line);
 
-			if (!string_contains_char(header_line, ':')) {
-				return PARSING_RES_NOT_ENOUGH_DATA;
-			}
+            if (!string_contains_char(header_line, ':')) {
+                return PARSING_RES_NOT_ENOUGH_DATA;
+            }
 
-			string header_name = eat_header_name(&header_line);
+            string header_name = eat_header_name(&header_line);
 
-			if (header_name.count == 0) {
-				return PARSING_RES_NOT_ENOUGH_DATA;
-			}
+            if (header_name.count == 0) {
+                return PARSING_RES_NOT_ENOUGH_DATA;
+            }
 
-			eat_whitespace(&header_line);
-			string header_value = header_line;
+            eat_whitespace(&header_line);
+            string header_value = header_line;
 
-			if (header_value.count == 0) {
-				return PARSING_RES_NOT_ENOUGH_DATA;
-			}
+            if (header_value.count == 0) {
+                return PARSING_RES_NOT_ENOUGH_DATA;
+            }
 
-			if (header_index < headers_max_len) {
-				headers_buf[header_index].name     = header_name.data;
-				headers_buf[header_index].name_len = header_name.count;
+            if (header_index < headers_max_len) {
+                headers_buf[header_index].name     = header_name.data;
+                headers_buf[header_index].name_len = header_name.count;
 
-				headers_buf[header_index].value     = header_value.data;
-				headers_buf[header_index].value_len = header_value.count;
-				
-				num_headers_written++;
-			} else {
-				// Ran out of memory.
-				return PARSING_RES_NOT_ENOUGH_MEMORY;
-			}
+                headers_buf[header_index].value     = header_value.data;
+                headers_buf[header_index].value_len = header_value.count;
 
-			header_line = eat_line(&text);
-			header_index++;
-		}
+                num_headers_written++;
+            } else {
+                // Ran out of memory.
+                return PARSING_RES_NOT_ENOUGH_MEMORY;
+            }
 
-		out_resp->headers     = headers_buf;
-		out_resp->headers_len = num_headers_written;
-	}
+            header_line = eat_line(&text);
+            header_index++;
+        }
 
-	// Remaining text is the body.
-	{
-		string body = text;
+        out_resp->headers     = headers_buf;
+        out_resp->headers_len = num_headers_written;
+    }
 
-		// Body is optional, so don't check for empty?
+    // Remaining text is the body.
+    {
+        string body = text;
 
-		out_resp->body     = body.data;
-		out_resp->body_len = body.count;
-	}
+        // Body is optional, so don't check for empty?
 
-	return PARSING_RES_SUCCEEDED;
+        out_resp->body     = body.data;
+        out_resp->body_len = body.count;
+    }
+
+    return PARSING_RES_SUCCEEDED;
 }
