feda3d2ceb4d6bc05560504829857ebca68ac626
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2024-07-20 08:36:56 +0300

    Reformat tests

--- test/main.c
+++ test/main.c
@@ -9,105 +9,6 @@
 // TODO: Test requests.
 // 
 
-static const char* responses[] = {
-    "HTTP/1.1 403 Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5, max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220\n"
-    "\n"
-    "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-    "2.0//EN\">\n"
-    "(more data)",
-
-    "FOOBAR 403 Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5, max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220\n"
-    "\n"
-    "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-    "2.0//EN\">\n"
-    "(more data)",
-
-    "HTTP/1.1 FOOBAR Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5, max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220\n"
-    "\n"
-    "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-    "2.0//EN\">\n"
-    "(more data)",
-
-    "HTTP/1.1 403 FOOBAR\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5 max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10 Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220\n"
-    "\n"
-    "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
-    "2.0//EN\">\n"
-    "(more data)",
-
-    "HTTP/1.1 403 Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5 max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10 Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220\n", // There's a newline, so this one is valid.
-
-    "HTTP/1.1 403 Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5, max=1000\n"
-    "Connection: Keep-Alive\n"
-    "Age: 3464\n"
-    "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
-    "X-Cache-Info: caching\n"
-    "Content-Length: 220", // No newline, so this one is invalid.
-
-    "HTTP/1.1 403 Forbidden\n"
-    "Server: Apache\n"
-    "Content-Type: text/html; charset=iso-8859-1\n"
-    "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
-    "Keep-Alive: timeout-=5, max=1000\n"
-    "Conn", // Incomplete header.
-
-    "HTTP/1.1 403 Forbidden\n"
-    "\n"
-    "this one is valid but has no headers",
-
-    "HTTP/1.1 403 Forbidden",
-
-    "HTTP/",
-};
-
 #define STR(s) ((string) {s, sizeof(s) - 1})
 
 typedef struct {
@@ -129,15 +30,28 @@ static bool strings_match(string a, string b) {
 
 #define my_assert(expr) do { while (!(expr)) { printf("ASSERTION FAILED: "__FILE__":%i: %s\n", __LINE__, #expr); exit(1); } } while (0)
 
-int main(int argc, char* argv[]) {
+static void test_response_full() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)";
+
     http_header_t headers_buf[100];
     http_response_t response;
-    http_parsing_result_t result;
-
-    // Test 0
-    result = http_parse_response(responses[0], strlen(responses[0]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_SUCCEEDED);
     my_assert(strings_match((string){response.protocol, response.protocol_len}, STR("HTTP/1.1")));
     my_assert(response.status_code == 403);
@@ -163,72 +77,203 @@ int main(int argc, char* argv[]) {
     my_assert(strings_match((string){response.headers[6].value, response.headers[6].value_len}, STR("Wed 10, Aug 2016 09:46:25 GMT")));
     my_assert(strings_match((string){response.headers[7].value, response.headers[7].value_len}, STR("caching")));
     my_assert(strings_match((string){response.headers[8].value, response.headers[8].value_len}, STR("220")));
+}
 
-    // Test 1
-    result = http_parse_response(responses[1], strlen(responses[1]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_invalid_protocol() {
+    char text[] = 
+        "FOOBAR 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)";
+
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_FAILED);
+}
 
-    // Test 2
-    result = http_parse_response(responses[2], strlen(responses[2]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_invalid_status_code() {
+    char text[] = 
+        "HTTP/1.1 FOOBAR Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_FAILED);
+}
 
-    // Test 3
-    result = http_parse_response(responses[3], strlen(responses[3]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_custom_status_text() {
+    char text[] = 
+        "HTTP/1.1 403 FOOBAR\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5 max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10 Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n"
+        "\n"
+        "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML\n"
+        "2.0//EN\">\n"
+        "(more data)";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_SUCCEEDED);
     my_assert(strings_match((string){response.protocol, response.protocol_len}, STR("HTTP/1.1")));
     my_assert(response.status_code == 403);
     my_assert(strings_match((string){response.status_text, response.status_text_len}, STR("FOOBAR")));
     my_assert(response.headers_len == 9);
+}
 
-    // Test 4
-    result = http_parse_response(responses[4], strlen(responses[4]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_header_with_newline() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5 max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10 Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220\n";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_SUCCEEDED);
     my_assert(strings_match((string){response.protocol, response.protocol_len}, STR("HTTP/1.1")));
     my_assert(response.status_code == 403);
     my_assert(strings_match((string){response.status_text, response.status_text_len}, STR("Forbidden")));
     my_assert(response.headers_len == 9);
+}
 
-    // Test 5
-    result = http_parse_response(responses[5], strlen(responses[5]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_header_no_newline() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Connection: Keep-Alive\n"
+        "Age: 3464\n"
+        "Date: Wed 10, Aug 2016 09:46:25 GMT\n"
+        "X-Cache-Info: caching\n"
+        "Content-Length: 220";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_NOT_ENOUGH_DATA);
+}
 
-    // Test 6
-    result = http_parse_response(responses[6], strlen(responses[6]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_incomplete_header() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden\n"
+        "Server: Apache\n"
+        "Content-Type: text/html; charset=iso-8859-1\n"
+        "Date: Wed, 10 Aug 2016 09:23:25 GMT\n"
+        "Keep-Alive: timeout-=5, max=1000\n"
+        "Conn";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_NOT_ENOUGH_DATA);
+}
 
-    // Test 7
-    result = http_parse_response(responses[7], strlen(responses[7]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_no_headers() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden\n"
+        "\n"
+        "this one is valid but has no headers";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_SUCCEEDED);
     my_assert(strings_match((string){response.protocol, response.protocol_len}, STR("HTTP/1.1")));
     my_assert(response.status_code == 403);
     my_assert(strings_match((string){response.status_text, response.status_text_len}, STR("Forbidden")));
     my_assert(response.headers_len == 0);
+}
 
-    // Test 8
-    result = http_parse_response(responses[8], strlen(responses[8]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_only_status_line() {
+    char text[] = 
+        "HTTP/1.1 403 Forbidden";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_NOT_ENOUGH_DATA);
+}
 
-    // Test 9
-    result = http_parse_response(responses[9], strlen(responses[9]),
-                                 headers_buf, ARRAY_LENGTH(headers_buf),
-                                 &response);
+static void test_response_only_incomplete_protocol() {
+    char text[] = 
+        "HTTP/";
+    
+    http_header_t headers_buf[100];
+    http_response_t response;
+    http_parsing_result_t result = http_parse_response(text, sizeof(text) - 1,
+                                                       headers_buf, ARRAY_LENGTH(headers_buf),
+                                                       &response);
     my_assert(result == PARSING_RES_NOT_ENOUGH_DATA);
+}
+
+int main(int argc, char* argv[]) {
+    test_response_full();
+    test_response_invalid_protocol();
+    test_response_invalid_status_code();
+    test_response_custom_status_text();
+    test_response_header_with_newline();
+    test_response_header_no_newline();
+    test_response_incomplete_header();
+    test_response_no_headers();
+    test_response_only_status_line();
+    test_response_only_incomplete_protocol();
 
     printf("All tests passed.\n");
 }
